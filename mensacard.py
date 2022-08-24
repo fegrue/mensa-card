7783710ba03bfe4be28589a606ed4c5ab12d04b2
Felix Grüning <fegrue@gmail.com>
2022-08-25 00:58:52 +0200

    geht

--- mensacard.py
+++ mensacard.py
@@ -4,23 +4,28 @@ import json
 import os
 
 
-print()
+import os
 
-file = open("buf.txt", "w+")
-if file.read() == "":
-    newcardid = input("Please enter your Card-ID from STW-Muenster: (eg.123456)")
-    file.write(newcardid)
+def file_is_empty(path):
+    return os.stat(path).st_size==0
+
+file = open("./buf.txt", "w")
+file.close()
+
+if file_is_empty("./buf.txt"):
+    file = open("./buf.txt", "w")
+    file.write(input("Please enter your Card-ID from STW-Muenster: (eg.123456)"))
+    file.close()
 
 
 
 
-
+file = open("./buf.txt", "r")
 cardnumber = file.read()
 file.close()
 link = f'https://api.topup.klarna.com/api/v1/STW_MUNSTER/cards/{cardnumber}/balance'
 page = requests.get(link)
 soup = str(BeautifulSoup(page.content, 'html.parser'))
-#soupstr = str(soup)
 data = json.loads(soup)
 
 balance = data["balance"]
